0f0dcba6799852d005340421600497176f36a208
jobkarani <jobkarani084j@gmail.com>
2022-09-19 10:53:45 +0300

    testimonials done

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ const closeMenu = () => {
 
 hamburger.addEventListener("click" , mobileMenu)
 navLink.forEach((l) => l.addEventListener("click",closeMenu))
+
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
