18160b7cecc35c005293a7425d7da8657bd5b71d
jobkarani <jobkarani084j@gmail.com>
2022-09-24 17:46:04 +0300

    slideshow

--- js/main.js
+++ js/main.js
@@ -15,44 +15,44 @@ const closeMenu = () => {
 hamburger.addEventListener("click" , mobileMenu)
 navLink.forEach((l) => l.addEventListener("click",closeMenu))
 
-$(".carousel").owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0:{
-      items:1,
-      nav: false
-    },
-    600:{
-      items:2,
-      nav: false
-    },
-    1000:{
-      items:3,
-      nav: false
-    }
+// $(".carousel").owlCarousel({
+//   margin: 20,
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeout: 2000,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0:{
+//       items:1,
+//       nav: false
+//     },
+//     600:{
+//       items:2,
+//       nav: false
+//     },
+//     1000:{
+//       items:3,
+//       nav: false
+//     }
+//   }
+// });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-});
-
-const slideshowImages = document.querySelectorAll(".intro-slideshow img");
-
-const nextImageDelay = 5000;
-let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
-
-// slideshowImages[currentImageCounter].style.display = "block";
-slideshowImages[currentImageCounter].style.opacity = 1;
-
-setInterval(nextImage, nextImageDelay);
-
-function nextImage() {
-  // slideshowImages[currentImageCounter].style.display = "none";
-  slideshowImages[currentImageCounter].style.opacity = 0;
-
-  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
-
-  // slideshowImages[currentImageCounter].style.display = "block";
-  slideshowImages[currentImageCounter].style.opacity = 1;
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 10000); 
 }
