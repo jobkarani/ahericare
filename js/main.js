75dde0ba39855bf270a14e5ec84c4e95da073117
jobkarani <jobkarani084j@gmail.com>
2022-09-24 20:31:41 +0300

    testimonials done

--- js/main.js
+++ js/main.js
@@ -54,5 +54,33 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 10000); 
+  setTimeout(showSlides, 5000); 
+}
+
+
+let carouselIndex = 1;
+showCarousels(carouselIndex);
+
+function plusSlides(n) {
+  showCarousels(carouselIndex += n);
+}
+
+function currentSlide(n) {
+  showCarousels(carouselIndex = n);
+}
+
+function showCarousels(n) {
+  let i;
+  let slides = document.getElementsByClassName("card");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {carouselIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[carouselIndex-1].style.display = "block";  
+  dots[carouselIndex-1].className += " active";
 }
